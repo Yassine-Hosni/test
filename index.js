95d4b7af6c6732bb9575cf49a127d39eb44805f6
Yassine-Hosni <110723495+Yassine-Hosni@users.noreply.github.com>
2024-02-07 02:03:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const Eris = require("eris");
+const keep_alive = require('./keep_alive.js')
+function isWeekdayAndBusinessHours() {
+    const currentDate = new Date();
+    const dayOfWeek = currentDate.getDay();
+    const currentHour = currentDate.getHours();
+
+    // Check if it's Monday to Friday and between 9 am and 5 pm
+    return (dayOfWeek >= 1 && dayOfWeek <= 5) && (currentHour >= 9 && currentHour < 17);
+}
+
+// Replace TOKEN with your bot account's token
+const bot = new Eris(process.env.token);
+
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+if (isWeekdayAndBusinessHours()) {
+bot.connect(); // Get the bot to connect to Discord;}
---
+++ keep_alive.js
@@ -0,0 +1,7 @@
+var http = require('http');
+const currentDate = new Date();
+
+http.createServer(function (req, res) {
+  res.write("I'm alive",currentDate.getHours());
+  res.end();
+}).listen(8080);
