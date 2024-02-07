d4a7017f46ffa461503c2d65bc991d3943f37fae
Yassine-Hosni <110723495+Yassine-Hosni@users.noreply.github.com>
2024-02-07 02:09:53 +0100

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -2,6 +2,6 @@ var http = require('http');
 const currentDate = new Date();
 
 http.createServer(function (req, res) {
-  res.write("I'm alive",currentDate.getHours());
+  res.write("I'm alive"+currentDate.getHours());
   res.end();
 }).listen(8080);
