bb6e80897efb0606c87a234428b3a81f1f5707e8
Yassine-Hosni <110723495+Yassine-Hosni@users.noreply.github.com>
2024-02-07 14:09:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const keep_alive = require('./keep_alive.js');
+const keep_alive = require('./keep_alive.js')
 function isWeekdayAndBusinessHours() {
     const currentDate = new Date();
     const dayOfWeek = currentDate.getDay();
     const currentHour = currentDate.getHours();
 
-    return (dayOfWeek >= 1 && dayOfWeek <= 5) && (currentHour >= 8 && currentHour < 16);
+    // Check if it's Monday to Friday and between 9 am and 5 pm
+    return (dayOfWeek >= 1 && dayOfWeek <= 5) && (currentHour >= 9 && currentHour < 17);
 }
+
+
 if (isWeekdayAndBusinessHours()) {
 const Eris = require("eris");
+// Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
-bot.on("error", (err) => {
-  console.error(err);
-});
 
-    bot.connect();
-}
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+    
+bot.connect();
