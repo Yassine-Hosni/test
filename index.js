031934294156966aa4ffca4aa19298cc44a4d89b
Yassine-Hosni <110723495+Yassine-Hosni@users.noreply.github.com>
2024-02-07 02:19:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
+
 function isWeekdayAndBusinessHours() {
     const currentDate = new Date();
     const dayOfWeek = currentDate.getDay();
@@ -15,5 +16,7 @@ const bot = new Eris(process.env.token);
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
+
 if (isWeekdayAndBusinessHours()) {
-bot.connect(); // Get the bot to connect to Discord;}
+    bot.connect(); // Get the bot to connect to Discord
+}
