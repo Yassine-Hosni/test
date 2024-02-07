1f2e67f965d3b778a9d775d53efd41c4a041dc85
Yassine-Hosni <110723495+Yassine-Hosni@users.noreply.github.com>
2024-02-07 09:19:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const Eris = require("eris");
-const keep_alive = require('./keep_alive.js');
-
 function isWeekdayAndBusinessHours() {
     const currentDate = new Date();
     const dayOfWeek = currentDate.getDay();
@@ -9,7 +6,8 @@ function isWeekdayAndBusinessHours() {
     return (dayOfWeek >= 1 && dayOfWeek <= 5) && (currentHour >= 8 && currentHour < 16);
 }
 if (isWeekdayAndBusinessHours()) {
-    
+const Eris = require("eris");
+const keep_alive = require('./keep_alive.js');
 const bot = new Eris(process.env.token);
 bot.on("error", (err) => {
   console.error(err);
