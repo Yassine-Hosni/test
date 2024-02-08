0d2d43295eadd422477594408589f822bcb161dd
Yassine-Hosni <110723495+Yassine-Hosni@users.noreply.github.com>
2024-02-08 10:27:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ function isWeekdayAndBusinessHours() {
     const currentDate = new Date();
     const dayOfWeek = currentDate.getDay();
     const currentHour = currentDate.getHours();
-
     return (dayOfWeek >= 1 && dayOfWeek <= 5) && (currentHour >= 8 && currentHour < 16);
 }
-
-
 if (isWeekdayAndBusinessHours()) {
 const Eris = require("eris");
 const bot = new Eris(process.env.token);
 bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+  console.error(err);
 });
-
-
-
-
-    
-    bot.connect(); // Get the bot to connect to Discord
+bot.connect();
 }
